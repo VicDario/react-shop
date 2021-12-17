5df5b5dd4b447ec2e4046e54b50c6e044a087efd
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-17 15:36:20 -0300

    Minor change

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,19 +7,19 @@ import RecoveryPassword from "@pages/RecoveryPassword";
 import Home from "@pages/Home";
 import NotFound from "@pages/NotFound";
 
-import '@styles/global.scss'
+import "@styles/global.scss";
 
 const App = () => {
-    return (
-            <Routes>
-                <Route path="/" element={ <Layout /> } >
-                    <Route index element={ <Home /> } />
-                    <Route path="login" element={ <Login /> } />
-                    <Route path="recovery-password" element={ <RecoveryPassword /> } />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-    );
-}
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path="login" element={<Login />} />
+				<Route path="recovery-password" element={<RecoveryPassword />} />
+			</Route>
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
+};
 
 export default App;
