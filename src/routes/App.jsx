55f10222cc9ee6fb023ffc405006ec77718fbcfe
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-17 15:33:01 -0300

    Fix imports

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import Layout from "../containers/Layout";
-import Login from "../containers/Login";
-import RecoveryPassword from "../containers/RecoveryPassword";
-import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
+import Layout from "@containers/Layout";
+import Login from "@pages/Login";
+import RecoveryPassword from "@pages/RecoveryPassword";
+import Home from "@pages/Home";
+import NotFound from "@/pages/NotFound";
 
-import '../styles/global.scss'
+import '@styles/global.scss'
 
 const App = () => {
     return (
