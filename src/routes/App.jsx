c9b81b51890c39639d2ba8c52d08f8299f7a8a0c
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-17 15:33:28 -0300

    Fix route

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,7 +5,7 @@ import Layout from "@containers/Layout";
 import Login from "@pages/Login";
 import RecoveryPassword from "@pages/RecoveryPassword";
 import Home from "@pages/Home";
-import NotFound from "@/pages/NotFound";
+import NotFound from "@pages/NotFound";
 
 import '@styles/global.scss'
 
