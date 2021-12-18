c27570e2faea13883fe179dd2de53a5f5e696bc0
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-18 18:05:12 -0300

    Fetch products list

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductItem from '@components/ProductItem';
+
+import axios from 'axios';
 
 import '@styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products?limit=40&offset=0';
+
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+
+	useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response.data);
+	}, []);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{
+					products.map((product, index) => {
+						return <ProductItem key={index} product={product} />
+					})
+				}
 			</div>
 		</section>
 	);
