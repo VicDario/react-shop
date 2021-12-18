c7cfb05dabfd0f889dd5fbd9a65ff72a50fed8cb
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-18 20:26:31 -0300

    Typos fixed and handleSubmit 90%

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,55 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import axios from 'axios';
 
 import '@styles/Login.scss';
 
 import logo from '@logos/logo_yard_sale.svg';
 
-const Login = () => {
-	return (
-		<div class="login">
-			<div class="form-container">
-				<img src={logo} alt="logo" class="logo" />
+const API = ''
 
-				<form action="/" class="form">
-					<label for="email" class="label">
+const Login = () => {
+	const form = useRef(null);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const formData = new FormData(form.current);
+		const data = {
+			email: formData.get('email'),
+			password: formData.get('password'),
+		}
+		const response = axios.post('https://api.escuelajs.co/api/v1/auth/login', data);
+	}
+	return (
+		<div className="login">
+			<div className="form-container">
+				<img src={logo} alt="logo" className="logo" />
+
+				<form onSubmit={handleSubmit} className="form" ref={form}>
+					<label htmlFor="email" className="label">
 						Email address
 					</label>
 					<input
 						type="text"
-						id="email"
+						name="email"
 						placeholder="platzi@example.com"
-						class="input input-email"
+						className="input input-email"
 					/>
 
-					<label for="password" class="label">
+					<label htmlFor="password" className="label">
 						Password
 					</label>
 					<input
 						type="password"
-						id="password"
+						name="password"
 						placeholder="*********"
-						class="input input-password"
+						className="input input-password"
 					/>
 
-					<input type="submit" value="Log in" class="primary-button login-button" />
+					<input type="submit" value="Log in" className="primary-button login-button" />
 					<a href="/">Forgot my password</a>
 				</form>
 
-				<button class="secondary-button signup-button">Sign up</button>
+				<button className="secondary-button signup-button">Sign up</button>
 			</div>
 		</div>
 	);
