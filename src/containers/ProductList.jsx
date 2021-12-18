ad0c27a0277e1962f557c60b651b1efdae7f18aa
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-18 18:40:02 -0300

    Custom hook

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import ProductItem from '@components/ProductItem';
-
-import axios from 'axios';
+import useGetProducts from '@hooks/useGetProducts';
 
 import '@styles/ProductList.scss';
 
 const API = 'https://api.escuelajs.co/api/v1/products?limit=40&offset=0';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, []);
+	const products = useGetProducts(API);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
 				{
-					products.map((product, index) => {
-						return <ProductItem key={index} product={product} />
+					products.map((product) => {
+						return <ProductItem key={product.id} product={product} />
 					})
 				}
 			</div>
