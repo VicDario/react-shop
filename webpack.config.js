031cd651ab6fec948597ad13b76ddf192ae66687
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-18 23:28:07 -0300

    Config for chunks (lazy render)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].[contenthash].bundle.js',
+        chunkFilename: '[name].[contenthash].bundle.js',
         publicPath: '/'
     },
     resolve: {
