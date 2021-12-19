bd1af22b8e3b9512b959420a92a985b3f0b50e2a
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-19 15:50:47 -0300

    Lazy loading for better performance added

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,37 +5,108 @@ import AppContext from '../context/AppContext';
 import useInitialState from '@hooks/useInitialState';
 
 import Layout from '@containers/Layout';
-import Login from '@pages/Login';
-import RecoveryPassword from '@pages/RecoveryPassword';
-import Home from '@pages/Home';
-import NotFound from '@pages/NotFound';
-import MyAccount from '@pages/MyAccount';
-import Orders from '@pages/Orders';
-import CreateAccount from '@pages/CreateAccount';
-import Checkout from '@pages/Checkout';
-import NewPassword from '@pages/NewPassword';
-import SendEmail from '@pages/SendEmail';
+
+const Login = lazy(() => import('@pages/Login'));
+const RecoveryPassword = lazy(() => import('@pages/RecoveryPassword'));
+const Home = lazy(() => import('@pages/Home'));
+const MyAccount = lazy(() => import('@pages/MyAccount'));
+const CreateAccount = lazy(() => import('@pages/CreateAccount'));
+const Orders = lazy(() => import('@pages/Orders'));
+const Checkout = lazy(() => import('@pages/Checkout'));
+const NewPassword = lazy(() => import('@pages/NewPassword'));
+const SendEmail = lazy(() => import('@pages/SendEmail'));
+const NotFound = lazy(() => import('@pages/NotFound'));
 
 import '@styles/global.scss';
 
 const App = () => {
 	const initialState = useInitialState();
 	return (
-		<AppContext.Provider value={initialState} >
-		<Routes>
-			<Route path="/" element={<Layout />}>
-				<Route index element={<Home />} />
-				<Route path="login" element={<Login />} />
-				<Route path="recovery-password" element={<RecoveryPassword />} />
-				<Route path="account" element={<MyAccount />} />
-				<Route path="signup" element={<CreateAccount />} />
-				<Route path="orders" element={<Orders />} />
-				<Route path="checkout" element={<Checkout />} />
-				<Route path="new-password" element={<NewPassword />} />
-				<Route path="send-email" element={<SendEmail />} />
-			</Route>
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+		<AppContext.Provider value={initialState}>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route
+						index
+						element={
+							<Suspense fallback={<>...</>}>
+								<Home />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="login"
+						element={
+							<Suspense fallback={<>...</>}>
+								<Login />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="recovery-password"
+						element={
+							<Suspense fallback={<>...</>}>
+								<RecoveryPassword />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="account"
+						element={
+							<Suspense fallback={<>...</>}>
+								<MyAccount />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="signup"
+						element={
+							<Suspense fallback={<>...</>}>
+								<CreateAccount />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="orders"
+						element={
+							<Suspense fallback={<>...</>}>
+								<Orders />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="checkout"
+						element={
+							<Suspense fallback={<>...</>}>
+								<Checkout />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="new-password"
+						element={
+							<Suspense fallback={<>...</>}>
+								<NewPassword />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="send-email"
+						element={
+							<Suspense fallback={<>...</>}>
+								<SendEmail />
+							</Suspense>
+						}
+					/>
+				</Route>
+				<Route
+					path="*"
+					element={
+						<Suspense fallback={<>...</>}>
+							<NotFound />
+						</Suspense>
+					}
+				/>
+			</Routes>
 		</AppContext.Provider>
 	);
 };
