e57b9bb06d06ea9a052ed9ee380a3abc620a9c6b
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-18 23:35:26 -0300

    now items can be added at cart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+
 import '@styles/ProductItem.scss';
 
-import addToCart from '@icons/bt_add_to_cart.svg';
+import addToCartImage from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = ({ product }) => {
+	const { addToCart } = useContext(AppContext);
+
+	const handleAddToCart = item => {
+		addToCart(item);
+	};
 
 	return (
 		<div className="ProductItem">
@@ -13,12 +20,12 @@ const ProductItem = ({ product }) => {
 					<p>${parseFloat(product.price).toFixed(2)}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure>
-					<img src={addToCart} alt="Add to cart" />
+				<figure onClick={() => handleAddToCart(product)} >
+					<img src={addToCartImage} alt="Add to cart" />
 				</figure>
 			</div>
 		</div>
 	);
-}
+};
 
 export default ProductItem;
