5990e2a83d08c999c0de09fd417aec46f125ad0a
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-19 02:04:20 -0300

    Context added and list of products

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import OrderItem from '@components/OrderItem';
+
+import AppContext from '../context/AppContext';
 
 import '@styles/MyOrder.scss';
 
 import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
+	const { state } = useContext(AppContext);
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -13,12 +16,14 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map((product) => {
+					return <OrderItem product={product} key={`orderItem-${product.id}`} />;
+				})}
 				<div className="order">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>$</p>
 				</div>
 				<button className="primary-button">Checkout</button>
 			</div>
