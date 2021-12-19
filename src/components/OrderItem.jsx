a3a69e699e921f4a3e442c63f42a935c17e3271b
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-19 02:03:57 -0300

    Changes for info product

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -4,14 +4,15 @@ import '@styles/OrderItem.scss';
 
 import close from '@icons/icon_close.png';
 
-const OrderItem = () => {
+const OrderItem = ({ product }) => {
+	console.log('hola');
 	return (
 		<div className="OrderItem">
 			<figure>
-				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+				<img src={product.images[0]} alt={product.title} />
 			</figure>
-			<p>Bike</p>
-			<p>$30,00</p>
+			<p>{product.title}</p>
+			<p>${parseFloat(product.price).toFixed(2)}</p>
 			<img src={close} alt="close" />
 		</div>
 	);
