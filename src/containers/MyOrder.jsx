a79934d17cfdedeb780c69f0872ba5f39483b6f3
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-19 02:11:09 -0300

    dynamic total added

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -9,6 +9,12 @@ import arrow from '@icons/flechita.svg';
 
 const MyOrder = () => {
 	const { state } = useContext(AppContext);
+
+	const sumTotal = () => {
+		const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	};
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -23,7 +29,7 @@ const MyOrder = () => {
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">Checkout</button>
 			</div>
