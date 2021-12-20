58b02e10726ee5e3847fbc6a85952e5ddf763775
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-19 23:53:24 -0300

    format

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import { HashRouter } from 'react-router-dom';
 import App from './routes/App';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <HashRouter basename='/'>
-            <App />
-        </ HashRouter>
-    </React.StrictMode>
-, document.getElementById('app'));
+	<React.StrictMode>
+		<HashRouter basename="/">
+			<App />
+		</HashRouter>
+	</React.StrictMode>,
+	document.getElementById('app')
+);
