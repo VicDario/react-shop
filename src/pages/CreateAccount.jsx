fcc6c2c73612c0b9b862d863e7799e272907a298
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-20 01:46:33 -0300

    Register

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,37 +1,64 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AppContext from '../context/AppContext';
+
 import '@styles/CreateAccount.scss';
 
+import axios from 'axios';
+
 const CreateAccount = () => {
+	const form = useRef(null);
+	const navigate = useNavigate();
+	const { setUser } = React.useContext(AppContext);
+
+	const handleRegister = async (e) => {
+		e.preventDefault();
+		const formData = new FormData(form.current);
+		const data = {
+			name: formData.get('name'),
+			email: formData.get('email'),
+			password: formData.get('password'),
+			role: 'customer'
+		};
+		console.log(data);
+		const response = axios.post('https://api.escuelajs.co/api/v1/user', data);
+		if(response.status === 201) {
+			alert('User created successfully');
+			setUser(response.data);
+			navigate('/login');
+		}
+		else alert('Error')
+	}
 	return (
 		<div className="CreateAccount">
 			<div className="CreateAccount-container">
 				<h1 className="title">My account</h1>
-				<form action="/" className="form">
+				<form onSubmit={handleRegister} className="form" ref={form}>
 					<div>
-						<label for="name" className="label">
+						<label htmlFor="name" className="label">
 							Name
 						</label>
 						<input
 							type="text"
-							id="name"
+							name="name"
 							placeholder="Teff"
 							className="input input-name"
 						/>
-						<label for="email" className="label">
+						<label htmlFor="email" className="label">
 							Email
 						</label>
 						<input
 							type="text"
-							id="email"
+							name="email"
 							placeholder="platzi@example.com"
 							className="input input-email"
 						/>
-						<label for="password" className="label">
+						<label htmlFor="password" className="label">
 							Password
 						</label>
 						<input
 							type="password"
-							id="password"
+							name="password"
 							placeholder="*********"
 							className="input input-password"
 						/>
