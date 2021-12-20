8721da94e838b35cb2a43f2820aefff5c5cbf96f
VicDario <26585356+VicDario@users.noreply.github.com>
2021-12-19 22:48:25 -0300

    change in chunks's name

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].[contenthash].bundle.js',
-		chunkFilename: '[name].[contenthash].bundle.js',
+		chunkFilename: '[name].[contenthash].chunk.js',
 		publicPath: '/',
 	},
 	resolve: {
