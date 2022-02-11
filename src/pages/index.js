181e1f27aaebb542516bfbd3b07394b16a7403ce
VicDario <26585356+VicDario@users.noreply.github.com>
2022-02-11 15:46:16 -0300

    Added PorductList at home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Header from '@components/Header';
+import ProductList from '@containers/ProductList';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ProductList />
     </>
   );
 }
